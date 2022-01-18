0e0951332e019a6ef5ddd35bfa1fc8cb99dc215f
Micdu70 <Micdu70@gmx.fr>
2022-01-18 12:30:04 +0100

    Update plugin

--- lang/en.js
+++ lang/en.js
@@ -1,14 +1,18 @@
 theUILang['flm_popup_flm-create-share'] =   'Create file share link';
 theUILang['flm_popup_fsh-view'] =   'View details';
+theUILang['flm_popup_fsh-view-fm'] =   'Show in File Manager';
 
 theUILang.FSshow		= 'File Share';
+theUILang.FSaddnew		= 'New share';
 theUILang.FSdel			= 'Delete link(s)';
 theUILang.FSdelmsg		= 'Do you really want to delete the selected items?';
 theUILang.FSvdur		= 'Please enter a valid duration';
 theUILang.FSfile		= 'File';
 theUILang.FSexpire		= 'Expire';
 theUILang.FSpassword		= 'Password';
+theUILang.FScreated		= 'Creation date';
 theUILang.FSdlink		= 'Download link';
+theUILang.FSdnumb		= 'Number of downloads';
 theUILang.FSdhours		= 'Duration (hours)';
 theUILang.FSadd			= 'Add';
 theUILang.FSedit		= 'Edit';
--- lang/fr.js
+++ lang/fr.js
@@ -1,22 +1,27 @@
-﻿theUILang['flm_popup_flm-create-share'] =   'Create file share link';
-theUILang['flm_popup_fsh-view'] =   'View details';
+theUILang['flm_popup_flm-create-share'] =   'Créer un lien de partage';
+theUILang['flm_popup_fsh-view'] =   'Voir les détails';
+theUILang['flm_popup_fsh-view-fm'] =   'Voir dans le Gestionnaire de fichiers';
 
-theUILang.FSshow		= 'File Share';
+﻿theUILang.FSshow		= 'Partage de fichiers';
+theUILang.FSaddnew		= 'Nouveau partage';
 theUILang.FSdel			= 'Supprimer le(s) liens(s)';
 theUILang.FSdelmsg		= 'Voulez-vous réellement supprimer ces éléments ?';
 theUILang.FSvdur		= 'Veuillez entrer une durée valide';
-theUILang.FSfile		= 'Fichier ';
-theUILang.FSexpire		= 'Expire';
-theUILang.FSpassword		= 'Mot de passe ';
+theUILang.FSfile		= 'Fichier';
+theUILang.FSexpire		= 'Date d\'expiration';
+theUILang.FSpassword		= 'Mot de passe';
+theUILang.FScreated		= 'Date de création';
 theUILang.FSdlink		= 'Lien de téléchargement';
-theUILang.FSdhours		= 'Durée (heure) ';
+theUILang.FSdnumb		= 'Nombre de téléchargements';
+theUILang.FSdhours		= 'Durée (heure)';
 theUILang.FSadd			= 'Ajouter';
 theUILang.FSedit		= 'Éditer';
-theUILang.FSshare		= 'Partager...';
+theUILang.FSshare		= 'Créer un lien de partage';
 theUILang.FSlinkcreate		= 'Lien de téléchargement créé';
 theUILang.FSlinkedit		= 'Lien de téléchargement édité';
 theUILang.FScopylink		= 'Copier le lien';
 theUILang.FSmaxdur		= 'Durée maxi autorisée est :';
 theUILang.FSnolimit		= '[Pas de limite est autorisée : 0]';
 theUILang.FSnolimitoff		= 'Durée illimitée n\'est pas autorisée';
+
 thePlugins.get("filemanager-share").langLoaded();
