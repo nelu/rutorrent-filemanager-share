08abb838ec1e76d59051e3da5f3be69afeef31a1
Micdu70 <Micdu70@gmx.fr>
2022-02-01 23:37:57 +0100

    Update password-form.php (#8)

--- views/password-form.php
+++ views/password-form.php
@@ -2,7 +2,7 @@
 // error section
 $err = $withError
 ? <<<ERR
-<h1 style="color: red;">Wrong password. Try again!</h1> 
+<div id="error" style="display: inline-block; color: red; margin-left: 8px;"><strong>Wrong password. Try again!</strong></div>
 ERR
 : '';
 
@@ -10,16 +10,40 @@ return <<<PAGE
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta name="viewport" content="width=device-width,initial-scale=1"/>
 <title>Password required</title>
+<style>
+#error {
+  -moz-animation: cssAnimation 0s ease-in 4s forwards;
+  -webkit-animation: cssAnimation 0s ease-in 4s forwards;
+  -o-animation: cssAnimation 0s ease-in 4s forwards;
+  animation: cssAnimation 0s ease-in 4s forwards;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+@keyframes cssAnimation {
+  to {
+    width:0;
+    height:0;
+    overflow:hidden;
+  }
+}
+@-webkit-keyframes cssAnimation {
+  to {
+    width:0;
+    height:0;
+    visibility:hidden;
+  }
+}
+</style>
 </head>
 <body>
-{$err}
 <h1>Password required</h1>
 <form method="post" onsubmit="setTimeout(function(){document.getElementById('pw').value=''},10)">
 <label for="pw">Password:</label> &nbsp;
 <input type="text" name="pw" id="pw" value="" style="-webkit-text-security: disc" autocomplete="off" required>
 <br><br>
-<input type="submit" name="submit">
+<div style="display: inline-block;"><input type="submit" name="submit">{$err}</div>
 </form>
 </body>
 </html>
