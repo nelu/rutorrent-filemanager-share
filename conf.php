f3efa95439da1c4bfab28ef9327f7f01edf83670
nelu <nelu@github.com>
2025-01-13 22:02:43 +0200

    url samples for endpoint config

--- conf.php
+++ conf.php
@@ -16,8 +16,9 @@ return [
 
     // url with the path where a symlink to share.php can be found
     // example: http://mydomain.com/share.php
+    // 'endpoint' = '//'.$_SERVER['HTTP_HOST'].'/rutorrent/plugins/filemanager-share/share.php',
     // relative url example
-    // 'endpoint' = '//'.$_SERVER['HTTP_HOST'].'/rutorrent/plugins/filemanager-share/share.php';
+    // 'endpoint' = './plugins/filemanager-share/share.php',
     'endpoint' => $_ENV['RU_FLM_SHARE_ENDPOINT'] ?? '',
 
     // key used for storing encrypted data
